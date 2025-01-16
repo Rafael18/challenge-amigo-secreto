031823c9db8f0646493d48f70d28483b210d6cc2
Rafael B <rafaeltec_@live.com>
2025-01-16 16:43:45 -0300

    Criado o array amigos, realizado a validação da entrada dos nomes, adição dos nomes no array e limpeza do campo input-amigs

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let nomeAmigo;
+function adicionarAmigo(){
+    nomeAmigo = document.getElementById('amigo').value;
+    
+    validarNomeAmigo(nomeAmigo);
+    limparInput();
+}
+
+function validarNomeAmigo(nome){
+    if (nome == ''){
+        alert('Por favor, insira um nome.')
+    }else {
+        atualizarArrayAmigo(nome);
+    }
+}
+
+function atualizarArrayAmigo(nome){
+    amigos.push(nome);
+    console.log(amigos);
+}
+
+function limparInput(){
+    console.log('Func limpar')
+    nomeAmigo = document.getElementById('amigo');
+    nomeAmigo.value = '';
+}
